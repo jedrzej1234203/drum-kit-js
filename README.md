c58a0a96c02690f343c9d60147ac3af4428df886
jedrzej1234203 <jedrzej.woloszyn@gmail.com>
2023-05-01 14:12:38 +0200

    version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # drum-kit-js
+
+- [About](#about)
+- [Features](#features)
+- [Technical Information](#technical_information)
+
+<a name="about"></a>
+## About
+This is drum kit project i made while learning JavaScript
+
+<a name="features"></a>
+## Features
+-Real drum sound on click
+-Drum history tracker
+-Copy History and Reset Button
+-Responsible Web Design down to 375px
+
+<a name="technical_information"></a>
+## Technical Information
+
+To run the site on your local machine
+
+1. Download or clone the repository onto your machine.
+2. Extract the .zip file.
+3. Open .html file in your browser.
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = document.getElementById("drum-kit-app");
 const buttonList = [
   ["w", "CLAP"],
-  ["d", "HIHAT"],
+  ["s", "HIHAT"],
+  ["d", "KICK"],
   ["f", "OPENHAT"],
   ["g", "BOOM"],
   ["h", "RIDE"],
@@ -9,6 +10,8 @@ const buttonList = [
   ["k", "TOM"],
   ["l", "TINK"],
 ];
+const tracker = document.getElementById("history");
+tracker.onclick = copyAndReset;
 
 buttonList.forEach((element) => {
   let newButton = document.createElement("button");
@@ -25,6 +28,12 @@ buttonList.forEach((element) => {
 
 window.addEventListener("keydown", playDrum);
 
+function randomButtonColor(classLetter) {
+  let button = document.querySelector(`.${classLetter}`);
+  rColor = `#${Math.floor(Math.random() * 1000000)}`;
+  button.style.background = rColor;
+}
+
 function playDrum(key) {
   if (key.length !== 1) {
     key = key.key;
@@ -33,34 +42,67 @@ function playDrum(key) {
     case "w":
       let clap = new Audio("assets/clap.wav");
       clap.play();
+      tracker.innerHTML += "Clap ";
+      randomButtonColor(key);
       break;
-    case "d":
+    case "s":
       let hiHat = new Audio("assets/HiHat.mp3");
       hiHat.play();
-    break;
+      tracker.innerHTML += "HiHat ";
+      randomButtonColor(key);
+      break;
+    case "d":
+      let kick = new Audio("assets/kick.mp3");
+      kick.play();
+      tracker.innerHTML += "Kick ";
+      randomButtonColor(key);
+      break;
     case "f":
       let openHat = new Audio("assets/OpenHiHat.mp3");
       openHat.play();
+      tracker.innerHTML += "OpenHiHat ";
+      randomButtonColor(key);
       break;
     case "g":
       let boom = new Audio("assets/boom.wav");
       boom.play();
-    break;
+      tracker.innerHTML += "Boom ";
+      randomButtonColor(key);
+      break;
     case "h":
       let ride = new Audio("assets/Ride.mp3");
       ride.play();
-    break;
+      tracker.innerHTML += "Ride ";
+      randomButtonColor(key);
+      break;
     case "j":
       let snare = new Audio("assets/Snare.mp3");
       snare.play();
-    break;
+      tracker.innerHTML += "Snare ";
+      randomButtonColor(key);
+      break;
     case "k":
       let tom = new Audio("assets/Tom.mp3");
       tom.play();
-    break;
+      tracker.innerHTML += "Tom ";
+      randomButtonColor(key);
+      break;
     case "l":
       let tink = new Audio("assets/tink.wav");
       tink.play();
-    break;
+      tracker.innerHTML += "Tink ";
+      randomButtonColor(key);
+      break;
   }
+}
+
+function copyAndReset() {
+  let trackerHistory = tracker.innerText;
+  navigator.clipboard
+    .writeText(trackerHistory.slice(20))
+    .then(alert("History coppied to clipboard"));
+  let panelBackground = document.querySelectorAll(".panel");
+  panelBackground.forEach((panel) => {
+    panel.style.background = "#424242";
+  });
 }
